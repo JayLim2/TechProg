debf4f2f7815b0ce05fde4c65cbb6be4642605ff
JayLim <ddx180874@gmail.com>
2018-11-25 00:35:27 +0400

    Начата разработка базовой логики работы с аватарами. Написаны основные шаблоны для сервера и клиента, которые дальше необходимо разрабатывать. Обновлены формы.

--- src/client/controllers/WaitingPlayersFormController.java
+++ src/client/controllers/WaitingPlayersFormController.java
@@ -1,4 +1,109 @@
 package client.controllers;
 
+import client.utils.Forms;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.WindowEvent;
+import javafx.util.Duration;
+
 public class WaitingPlayersFormController {
+    @FXML
+    private TextField minutesField;
+    @FXML
+    private TextField secondsField;
+
+    private static final int WAIT_TIME_LIMIT_SECONDS = 10;
+
+    public void initialize() {
+        updateTimeFields();
+    }
+
+    public void showAction(WindowEvent event) {
+        resetTime();
+        playCountdown();
+        waitPlayers();
+    }
+
+    public void hideAction(WindowEvent event) {
+        //resetTime();
+    }
+
+    private static final Duration ONE_SECOND_DURATION = Duration.seconds(1);
+    private static final Duration UPDATE_INFO_INTERVAL = Duration.seconds(5);
+    private Integer totalSeconds = WAIT_TIME_LIMIT_SECONDS;
+
+    private void playCountdown() {
+        Timeline timeline = new Timeline();
+
+        KeyFrame frame = new KeyFrame(ONE_SECOND_DURATION, new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                totalSeconds--;
+                updateTimeFields();
+
+                if (totalSeconds <= 0) {
+                    playGame();
+                    timeline.stop();
+                    timeline.getKeyFrames().clear();
+                }
+            }
+        });
+
+        timeline.getKeyFrames().add(frame);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    /**
+     * Ожидание игроков
+     */
+    private void waitPlayers() {
+        // TODO: 24.11.2018
+        /*
+        Регулярно (например, раз в 5 сек)
+        проверять состояние сессии на сервере и в случае изменения
+        обновлять интерфейс актуальными данными
+         */
+        Timeline timeline = new Timeline();
+
+        KeyFrame frame = new KeyFrame(UPDATE_INFO_INTERVAL, new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println(totalSeconds);
+
+                if (totalSeconds <= 0) {
+                    timeline.stop();
+                    timeline.getKeyFrames().clear();
+                }
+            }
+        });
+
+        timeline.getKeyFrames().add(frame);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    private void resetTime() {
+        totalSeconds = WAIT_TIME_LIMIT_SECONDS;
+    }
+
+    private void updateTimeFields() {
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds - minutes * 60;
+        minutesField.setText(Integer.toString(minutes));
+        secondsField.setText(Integer.toString(seconds));
+    }
+
+    private void updateInfo() {
+        // TODO: 24.11.2018 реализовать
+    }
+
+    private void playGame() {
+        Forms.openForm("GameField");
+        Forms.closeForm("WaitingPlayers");
+    }
 }
--- src/client/utils/Forms.java
+++ src/client/utils/Forms.java
@@ -1,11 +1,15 @@
 package client.utils;
 
+import client.controllers.WaitingPlayersFormController;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,6 +23,7 @@ import java.util.stream.Stream;
 public class Forms {
     private static Map<String, Stage> formStages;
     private static final Pattern pattern = Pattern.compile("([\\w]*).fxml");
+    private static final String CSS_PATH = "/client/assets/css/main.css";
 
     public static void initialize() {
         if (formStages == null)
@@ -26,21 +31,46 @@ public class Forms {
 
         Path root = Paths.get("src/client");
         Path start = Paths.get(root + "/forms");
-        try (Stream<Path> pathStream = Files.walk(start)) {
+        try (Stream<Path> pathStream = Files.walk(start, 1)) {
             pathStream.skip(1).forEach(new Consumer<Path>() {
                 @Override
                 public void accept(Path path) {
                     String pathStr = "..\\" + root.relativize(path).toString();
                     System.out.println(pathStr);
                     try {
-                        Stage stage = new Stage();
-                        Parent root = FXMLLoader.load(Forms.class.getResource(pathStr));
-                        Scene scene = new Scene(root);
-                        stage.setScene(scene);
                         Matcher matcher = pattern.matcher(path.toString());
                         if (matcher.find()) {
-                            formStages.putIfAbsent(matcher.group(1), stage);
-                            System.out.println("FORM NAME: " + matcher.group(1));
+                            String formName = matcher.group(1);
+                            URL resourceURL = getClass().getResource(pathStr);
+                            if (formName != null) {
+                                Stage stage = new Stage();
+                                Parent root;
+                                if (formName.equals("WaitingPlayers")) {
+                                    FXMLLoader fxmlLoader = new FXMLLoader(resourceURL);
+                                    root = fxmlLoader.load();
+                                    WaitingPlayersFormController controller = fxmlLoader.getController();
+                                    stage.addEventHandler(WindowEvent.WINDOW_SHOWN, new EventHandler<WindowEvent>() {
+                                        @Override
+                                        public void handle(WindowEvent window) {
+                                            controller.showAction(window);
+                                        }
+                                    });
+                                    stage.addEventHandler(WindowEvent.WINDOW_HIDDEN, new EventHandler<WindowEvent>() {
+                                        @Override
+                                        public void handle(WindowEvent window) {
+                                            controller.hideAction(window);
+                                        }
+                                    });
+                                } else {
+                                    root = FXMLLoader.load(resourceURL);
+                                }
+                                Scene scene = new Scene(root);
+                                stage.setScene(scene);
+                                addStyle(stage);
+                                formStages.putIfAbsent(formName, stage);
+
+                                System.out.println("FORM NAME: " + formName);
+                            }
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -50,6 +80,10 @@ public class Forms {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void addStyle(Stage stage) {
+        stage.getScene().getStylesheets().add(CSS_PATH);
     }
 
     public static void registerForm(String formName, Stage stage) {
@@ -70,5 +104,9 @@ public class Forms {
         if (stage != null) {
             stage.close();
         }
+    }
+
+    public static Stage getStage(String formName) {
+        return formStages.get(formName);
     }
 }
