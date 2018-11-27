2b854b2d20d4395b5f08e98554c105dd9594d2d9
Sergei Komarov <ddx180874@gmail.com>
2018-11-27 10:56:24 +0400

    GameField initialized

--- Client/src/main/java/samara/university/client/controllers/GameFieldFormController.java
+++ Client/src/main/java/samara/university/client/controllers/GameFieldFormController.java
@@ -1,5 +1,78 @@
 package samara.university.client.controllers;
 
-public class GameFieldFormController {
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
+public class GameFieldFormController {
+    //main info
+    private ImageView avatar1;
+    private ImageView avatar2;
+    private ImageView avatar3;
+    private ImageView myAvatar;
+
+    private Text labelLogin1;
+    private Text labelLogin2;
+    private Text labelLogin3;
+    private Text labelMyLogin;
+
+    private ImageView seniorMarker1;
+    private ImageView seniorMarker2;
+    private ImageView seniorMarker3;
+    private ImageView mySeniorMarker;
+
+    //total resources now
+    private Text labelMoney1;
+    private Text labelMoney2;
+    private Text labelMoney3;
+    private Text labelMyMoney;
+
+    private Text labelResources1;
+    private Text labelResources2;
+    private Text labelResources3;
+    private Text labelMyResources;
+
+    private Text labelProducts1;
+    private Text labelProducts2;
+    private Text labelProducts3;
+    private Text labelMyProducts;
+
+    private Text labelFactories1;
+    private Text labelFactories2;
+    private Text labelFactories3;
+    private Text labelMyFactories;
+
+    private Text labelAutoFactories1;
+    private Text labelAutoFactories2;
+    private Text labelAutoFactories3;
+    private Text labelMyAutoFactories;
+
+    //resources in progress
+    private Text labelProductsInProgress1;
+    private Text labelProductsInProgress2;
+    private Text labelProductsInProgress3;
+    private Text labelMyProductsInProgress;
+
+    private Text labelFactoriesInProgress1;
+    private Text labelFactoriesInProgress2;
+    private Text labelFactoriesInProgress3;
+    private Text labelMyFactoriesInProgress;
+
+    private Text labelAutoFactoriesInProgress1;
+    private Text labelAutoFactoriesInProgress2;
+    private Text labelAutoFactoriesInProgress3;
+    private Text labelMyAutoFactoriesInProgress;
+
+    private Text labelMyMoneyInProgress;
+
+    //bank
+    private Text labelBankResourcesCount;
+    private Text labelBankResourcesPrice;
+
+    private Text labelBankProductsCount;
+    private Text labelBankProdductsPrice;
+
+    //others
+    private Text labelMonth;
+    private Text labelPhase;
+    private Text timeLeft;
 }
